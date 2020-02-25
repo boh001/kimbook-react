3c01288c8af6687d8967c398a145bbeee689bbe0
boh001 <boh001g@gmail.com>
2020-02-25 23:45:43 +0900

    2020-02-25

--- src/Routes/Profile/ProfilePresenter.js
+++ src/Routes/Profile/ProfilePresenter.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Headers from "Components/Header";
 import { color } from "Components/variable";
+import { Link } from "react-router-dom";
 
 const ProfileFrame = styled.div`
   width: 100%;
@@ -13,11 +14,12 @@ const ProfileCover = styled.div`
   width: 850px;
 `;
 const ProfileHeader = styled.div`
+  position: relative;
   margin-top: 40px;
   height: 250px;
   border: 1px solid ${color.fbLine};
+  border-radius: ${color.frRa};
 `;
-const PorfileMain = styled.div``;
 const HeaderBg = styled.img.attrs({
   src:
     "https://kimbookreact.s3.ap-northeast-2.amazonaws.com/kim/image/1581935426485"
@@ -27,10 +29,14 @@ const HeaderBg = styled.img.attrs({
   object-fit: cover;
   object-position: 50% 15%;
 `;
-const HeaderAvatar = styled.img`
+const HeaderAvatar = styled.img.attrs({
+  src:
+    "https://kimbookreact.s3.ap-northeast-2.amazonaws.com/kim/image/1581935426485"
+})`
   position: absolute;
-  top: 140px;
-  right: 1660px;
+  padding: 5px;
+  top: 100px;
+  right: 665px;
   width: 170px;
   height: 170px;
   border-radius: 50%;
@@ -38,9 +44,57 @@ const HeaderAvatar = styled.img`
 `;
 const HeaderLists = styled.div`
   display: grid;
-  height: 40px;
-  grid-template-columns: repeat(7, 1fr);
+  height: 50px;
+  grid-template-columns: 1fr repeat(5, 100px) 1fr;
   border: 1px solid ${color.fbLine};
+  border-top: none;
+  border-radius: ${color.frRa};
+
+  &:nth-child(2) {
+    background-color: ${color.fbWhite};
+    border-left: 1px solid ${color.fbLine};
+    border-right: 1px solid ${color.fbLine};
+  }
+`;
+
+const HiddenList = styled.div`
+  display: grid;
+  padding: 10px 0px 10px 0px;
+  grid-gap: 10px;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  grid-template-columns: (5, 1fr);
+  position: absolute;
+  top: 339px;
+  background-color: ${color.fbWhite};
+  border: 1px solid ${color.fbLine};
+  visibility: hidden;
+  opacity: 0;
+  div {
+    width: 100%;
+    &:hover {
+      background-color: ${color.fbBg};
+    }
+  }
+`;
+const ListsUL = styled(Link)`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${color.fbWhite};
+  border-left: 1px solid ${color.fbLine};
+  color: ${color.fbBlue};
+  font-weight: bold;
+  &:hover {
+    background-color: ${color.fbBg};
+  }
+  &:hover ${HiddenList} {
+    visibility: visible;
+    opacity: 1;
+    transition: all ease 1s;
+  }
 `;
 export default () => (
   <>
@@ -52,13 +106,22 @@ export default () => (
           <HeaderAvatar />
         </ProfileHeader>
         <HeaderLists>
-          <div>hi</div>
-          <div>hi</div>
-          <div>hi</div>
-          <div>hi</div>
-          <div>hi</div>
-          <div>hi</div>
-          <div>hi</div>
+          <div></div>
+          <ListsUL>타임라인</ListsUL>
+          <ListsUL>정보</ListsUL>
+          <ListsUL>친구</ListsUL>
+          <ListsUL>사진</ListsUL>
+          <ListsUL>
+            더보기
+            <HiddenList>
+              <div>책</div>
+              <div>좋아요</div>
+              <div>이벤트</div>
+              <div>음악</div>
+              <div>영화</div>
+            </HiddenList>
+          </ListsUL>
+          <div></div>
         </HeaderLists>
       </ProfileCover>
     </ProfileFrame>
